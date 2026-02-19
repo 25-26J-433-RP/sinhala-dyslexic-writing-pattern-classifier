ed29c4041cbc0fdbfedffabdaeedb2bcd187617c
tinykav <kaveeshatiny@gmail.com>
2026-02-19 22:15:27 +0530

    Fix import path for Docker compatibility (use relative import)

--- writing_pattern_classifier/src/router.py
+++ writing_pattern_classifier/src/router.py
@@ -3,7 +3,7 @@ USE_VERSION = "v2"
 
 def analyze_essay(essay_text):
     if USE_VERSION == "v1":
-        from src.v1.pipeline import analyze_essay as analyze
+        from .v1.pipeline import analyze_essay as analyze
     else:
         from .v2.pipeline import analyze_essay as analyze
 
