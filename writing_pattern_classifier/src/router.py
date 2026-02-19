908d06f2e3f5d83e319e2ed878cdec43d52f46dc
tinykav <kaveeshatiny@gmail.com>
2026-02-19 22:05:28 +0530

    fixed the path issue on router file

--- writing_pattern_classifier/src/router.py
+++ writing_pattern_classifier/src/router.py
@@ -5,6 +5,8 @@ def analyze_essay(essay_text):
     if USE_VERSION == "v1":
         from src.v1.pipeline import analyze_essay as analyze
     else:
-        from src.v2.pipeline import analyze_essay as analyze
+        from .v2.pipeline import analyze_essay as analyze
+
+
 
     return analyze(essay_text)
